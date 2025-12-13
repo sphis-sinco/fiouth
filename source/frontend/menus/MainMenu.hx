56bb451c7b2ea8bdb9b213f4bfed66b9cd7f32c4
Viwu <sinconsistencia@gmail.com>
2025-12-12 23:53:14 -0600

    play text

--- source/frontend/menus/MainMenu.hx
+++ source/frontend/menus/MainMenu.hx
@@ -7,19 +7,29 @@ class MainMenu extends State
 {
 	public var title:FlxText;
 
+	public var playText:FlxText;
+
 	override public function create()
 	{
 		super.create();
 
 		title = new FlxText();
 		title.text = "Fiouth";
-
 		title.size = 32;
-		
+
 		title.screenCenter();
 		title.y -= title.height * 4;
 
 		add(title);
+
+		playText = new FlxText();
+		playText.text = "Play";
+		playText.size = 24;
+
+		playText.screenCenter();
+		playText.y += playText.height * 2;
+
+		add(playText);
 	}
 
 	override public function update(elapsed:Float)

768ca522d01fdaeb2e1d0d5c8f487de273f1f66a
Viwu <sinconsistencia@gmail.com>
2025-12-13 05:30:14 -0600

    work in progress text for the options menu

--- source/frontend/menus/SettingsMenu.hx
+++ source/frontend/menus/SettingsMenu.hx
@@ -1,5 +1,16 @@
 package frontend.menus;
 
+import flixel.text.FlxText;
 import backend.State;
 
-class SettingsMenu extends State {}
+class SettingsMenu extends State
+{
+	override function create()
+	{
+		super.create();
+
+        var wipText:FlxText = new FlxText(2, version.y + ((displayVersion) ? version.height : 0), 0, "[[Work in Progress]]", 16);
+        add(wipText);
+        wipText.alpha = version.alpha;
+	}
+}

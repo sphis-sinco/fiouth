e273e46fa5a721e067b699dec15b469b49253b96
Viwu <sinconsistencia@gmail.com>
2025-12-13 05:52:46 -0600

    performNullChecksOnSave function

--- source/backend/save/Save.hx
+++ source/backend/save/Save.hx
@@ -13,6 +13,27 @@ class Save
 		return data?.slot ?? 0;
 	}
 
+	public static function getDefault():RawSaveData
+	{
+		return {
+			version: Application.current.meta.get('version'),
+			slot: 1
+		}
+	}
+
+	public static function save()
+	{
+		FlxG.save.mergeData(data, true);
+		FlxG.save.flush();
+
+		trace('Saved');
+	}
+
+	public static function init()
+	{
+		loadFromSlot(1);
+	}
+
 	public static function loadFromSlot(slot:Int = 1)
 	{
 		FlxG.save.bind('Fiouth/Slot$slot', Application.current.meta.get('company'));
@@ -27,31 +48,23 @@ class Save
 				data = FlxG.save.data;
 		}
 
-        if (data.slot == null) data.slot = slot;
-        if (data.version == null) data.version = getDefault().version;
-		
-        trace('Save : ' + data);
-        save();
+		if (data.slot == null)
+		{
+			trace('Missing slot field');
+			data.slot = slot;
+		}
+		performNullChecksOnSave();
+
+		trace('Save : ' + data);
+		save();
 	}
 
-	public static function getDefault():RawSaveData
+	public static function performNullChecksOnSave()
 	{
-		return {
-			version: Application.current.meta.get('version'),
-			slot: 1
+		if (data.version == null)
+		{
+			trace('Missing version field');
+			data.version = getDefault().version;
 		}
 	}
-
-    public static function save()
-    {
-        FlxG.save.mergeData(data, true);
-        FlxG.save.flush();
-
-        trace('Saved');
-    }
-
-	public static function init()
-    {
-        loadFromSlot(1);
-    }
 }

bad683c6135f3787dab82464b99e6b682056d46a
Viwu <sinconsistencia@gmail.com>
2025-12-13 06:09:55 -0600

    welcome.

--- source/frontend/gameplay/GameplayStartingScene.hx
+++ source/frontend/gameplay/GameplayStartingScene.hx
@@ -1,5 +1,38 @@
 package frontend.gameplay;
 
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.text.FlxText;
 import backend.State;
 
-class GameplayStartingScene extends State {}
+class GameplayStartingScene extends State
+{
+	public var welcome:FlxText;
+
+	override function create()
+	{
+		super.create();
+
+		welcome = new FlxText();
+
+		welcome.text = "Welcome.";
+		welcome.size = 128;
+
+		welcome.screenCenter();
+
+		add(welcome);
+
+		welcome.alpha = 0;
+
+		FlxTween.tween(welcome, {alpha: 1}, 1.0, {
+			ease: FlxEase.sineInOut
+		});
+
+		playDialogueSound();
+	}
+
+	public function playDialogueSound()
+	{
+		FlxG.sound.play('assets/sounds/dialogue.wav', FlxG.random.float(0, .5));
+	}
+}

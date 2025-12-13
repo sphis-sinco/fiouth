7a61769d0008cea17fef815f267effb2467b33da
Viwu <sinconsistencia@gmail.com>
2025-12-13 08:17:27 -0600

    first choice path

--- source/backend/gameplay/GameplayPaths.hx
+++ source/backend/gameplay/GameplayPaths.hx
@@ -4,5 +4,7 @@ enum abstract GameplayPaths(String) from String to String {
     var START = './';
     var START_OLD = '.';
 
+    var FIRST_CHOICE = './first_choice/';
+
     var FAKE_END = 'end';
 }

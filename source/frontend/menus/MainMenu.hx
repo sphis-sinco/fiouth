0a4b42ad3e6ad343eefa5878ebad18c99a6716a4
Viwu <sinconsistencia@gmail.com>
2025-12-12 23:52:10 -0600

    title text

--- source/frontend/menus/MainMenu.hx
+++ source/frontend/menus/MainMenu.hx
@@ -1,12 +1,25 @@
 package frontend.menus;
 
+import flixel.text.FlxText;
 import backend.State;
 
 class MainMenu extends State
 {
+	public var title:FlxText;
+
 	override public function create()
 	{
 		super.create();
+
+		title = new FlxText();
+		title.text = "Fiouth";
+
+		title.size = 32;
+		
+		title.screenCenter();
+		title.y -= title.height * 4;
+
+		add(title);
 	}
 
 	override public function update(elapsed:Float)

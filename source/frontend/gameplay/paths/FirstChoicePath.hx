2e753a789a6e8bdb24e77cc56ccb94be65793667
Viwu <sinconsistencia@gmail.com>
2025-12-13 08:33:11 -0600

    the worst option

--- source/frontend/gameplay/paths/FirstChoicePath.hx
+++ source/frontend/gameplay/paths/FirstChoicePath.hx
@@ -19,6 +19,9 @@ class FirstChoicePath extends PathState
 
 	public var selection:Int = 0;
 
+	public var yes:FlxText;
+	public var no:FlxText;
+
 	override function create()
 	{
 		super.create();
@@ -40,6 +43,73 @@ class FirstChoicePath extends PathState
 
 		dialog.alpha = 0;
 		setDialogueText('Pick the right choice for <blue>your<blue> <cyan>people<cyan>.');
+
+		yes = new FlxText();
+		no = new FlxText();
+
+		yes.text = 'yes';
+		no.text = 'no';
+
+		yes.size = 32;
+		no.size = 32;
+
+		yes.screenCenter();
+		no.screenCenter();
+
+		yes.y -= yes.height;
+		no.y -= no.height;
+
+		add(yes);
+		add(no);
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		if (FlxG.keys.anyJustReleased([UP, W, DOWN, S, ENTER]))
+		{
+			if (FlxG.keys.anyJustReleased([UP, W]))
+				selection--;
+			if (FlxG.keys.anyJustReleased([DOWN, S]))
+				selection++;
+
+			if (selection < 0)
+				selection = 0;
+			if (selection > 1)
+				selection = 1;
+
+			yes.color = FlxColor.WHITE;
+			no.color = FlxColor.WHITE;
+
+			if (selection == 0)
+				yes.color = FlxColor.YELLOW;
+			if (selection == 1)
+				no.color = FlxColor.YELLOW;
+
+			if (FlxG.keys.anyJustReleased([ENTER]))
+			{
+				switch (selection)
+				{
+					case 0:
+						setDialogueText('You have done well.');
+					case 1:
+						setDialogueText('...');
+
+                        FlxTween.tween(yes, {alpha: 0}, 6);
+                        FlxTween.tween(no, {alpha: 0}, 6);
+
+						FlxG.sound.music.fadeOut(3, 0, t ->
+						{
+							FlxG.sound.playMusic('butYouPickedTheWorstOption'.musicPath());
+							FlxG.sound.music.fadeIn(3, 0, 1, t ->
+							{
+								setDialogueText('Why did you do that?');
+							});
+						});
+				}
+			}
+		}
 	}
 
 	public function setDialogueText(text:String)

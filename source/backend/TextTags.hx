57d0d631e29cc1f5a75f2a9c5860d7c49c52c67c
Viwu <sinconsistencia@gmail.com>
2025-12-13 06:37:40 -0600

    text colors :D

---
+++ source/backend/TextTags.hx
@@ -0,0 +1,27 @@
+package backend;
+
+import flixel.text.FlxText;
+
+class TextTags
+{
+	public static function apply(flxtext:FlxText)
+	{
+		flxtext.applyMarkup(flxtext.text, [
+			new FlxTextFormatMarkerPair(new FlxTextFormat(0xFF2222), "<red>"),
+			new FlxTextFormatMarkerPair(new FlxTextFormat(0xFF7722), "<orange>"),
+			new FlxTextFormatMarkerPair(new FlxTextFormat(0xFFFF22), "<yellow>"),
+			new FlxTextFormatMarkerPair(new FlxTextFormat(0x22FF22), "<green>"),
+			new FlxTextFormatMarkerPair(new FlxTextFormat(0x2222FF), "<blue>"),
+			new FlxTextFormatMarkerPair(new FlxTextFormat(0x7722FF), "<purple>"),
+			new FlxTextFormatMarkerPair(new FlxTextFormat(0xFF22FF), "<pink>"),
+			new FlxTextFormatMarkerPair(new FlxTextFormat(0x884422), "<brown>"),
+			new FlxTextFormatMarkerPair(new FlxTextFormat(0x999999), "<gray>"),
+			new FlxTextFormatMarkerPair(new FlxTextFormat(0x999999), "<grey>"),
+			new FlxTextFormatMarkerPair(new FlxTextFormat(0xCCCCCC), "<white>"),
+			new FlxTextFormatMarkerPair(new FlxTextFormat(0x222222), "<black>"),
+
+			new FlxTextFormatMarkerPair(new FlxTextFormat(0x22FFFF), "<cyan>"),
+
+		]);
+	}
+}

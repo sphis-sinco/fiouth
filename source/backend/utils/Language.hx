8c933bc63cf10d8fb1815978f865da9192c37504
Viwu <sinconsistencia@gmail.com>
2025-12-14 15:22:49 -0600

    remove _

--- source/backend/utils/Language.hx
+++ source/backend/utils/Language.hx
@@ -9,8 +9,8 @@ class Language
 	static function get_DEFAULT_LANGUAGE():String
 		return LANGUAGES[0] ?? 'en-us';
 
-	public static var LANGUAGE:String = DEFAULT_LANGUAGE + '_';
-
+	public static var LANGUAGE:String = DEFAULT_LANGUAGE;
+    
 	public static var LANGUAGES(get, never):Array<String>;
 
 	static function get_LANGUAGES():Array<String>

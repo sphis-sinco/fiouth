fdb9499074ac2909652655ac33fccd3451792536
Viwu <sinconsistencia@gmail.com>
2025-12-16 16:42:00 -0600

    bluespike_move event

--- source/frontend/gameplay/scenes/firstchoiceno/SneakPastTheArmy.hx
+++ source/frontend/gameplay/scenes/firstchoiceno/SneakPastTheArmy.hx
@@ -1,5 +1,6 @@
 package frontend.gameplay.scenes.firstchoiceno;
 
+import flixel.tweens.FlxTween;
 import flixel.util.FlxTimer;
 import backend.utils.Dialog;
 import backend.Sprite;
@@ -20,8 +21,11 @@ class SneakPastTheArmy extends PathState
 	}
 
 	public var commander:Sprite;
+	public var bluespike:Sprite;
 
 	public var intro_dialog:Array<String>;
+
+	public var playing:Bool = false;
 
 	override function create()
 	{
@@ -35,7 +39,39 @@ class SneakPastTheArmy extends PathState
 
 		commander.screenCenter();
 
+		bluespike = new Sprite(0, 0, commander.scaleOffset);
+		bluespike.loadGraphic('characters/sprites/army/bluespike-idle'.imagePath());
+		add(bluespike);
+
+		bluespike.screenCenter();
+
+		bluespike.x += bluespike.width * 4;
+
 		readDialogueList(intro_dialog);
+	}
+
+	override function eventFunction(events:Array<String>)
+	{
+		super.eventFunction(events);
+
+		if (events.contains('bluespike_move'))
+		{
+			var tick = 0;
+			FlxTween.tween(bluespike, {x: commander.x + bluespike.width * 2}, 1, {
+				onUpdate: function(t:FlxTween)
+				{
+					tick++;
+
+					if (tick % 10 == 0)
+					{
+						if (bluespike.graphic.key.endsWith('walk.png'))
+							bluespike.loadGraphic('characters/sprites/army/bluespike-idle'.imagePath());
+						else
+							bluespike.loadGraphic('characters/sprites/army/bluespike-walk'.imagePath());
+					}
+				}
+			});
+		}
 	}
 
 	override function setDialogueTextNoFade(text:String)

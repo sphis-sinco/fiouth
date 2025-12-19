1acefafdcabe8eabbbbc1e28acb193cda29f2362
Viwu <sinconsistencia@gmail.com>
2025-12-18 18:43:44 -0600

    buttons

--- source/frontend/gameplay/scenes/firstchoiceyes/GiveConfirmation.hx
+++ source/frontend/gameplay/scenes/firstchoiceyes/GiveConfirmation.hx
@@ -1,5 +1,6 @@
 package frontend.gameplay.scenes.firstchoiceyes;
 
+import backend.Sprite;
 import backend.gameplay.PathState;
 
 class GiveConfirmation extends PathState
@@ -7,5 +8,27 @@ class GiveConfirmation extends PathState
 	override public function new()
 	{
 		super(GIVE_CONFIRMATION);
+	}
+
+	public var confirm:Sprite;
+	public var deny:Sprite;
+
+	override function create()
+	{
+		super.create();
+
+		confirm = new Sprite();
+		confirm.loadGraphic('objects/confirmationButton/confirm'.imagePath());
+		add(confirm);
+
+		deny = new Sprite();
+		deny.loadGraphic('objects/confirmationButton/deny'.imagePath());
+		add(deny);
+
+		confirm.screenCenter();
+		confirm.x -= confirm.width;
+		
+		deny.screenCenter();
+		deny.x += deny.width;
 	}
 }
